c18e84a39b57a8a8ee1ec907e5b3d68769f48456
Taha Toklucu <tahatoklucu@hotmail.com>
2026-05-31 16:48:29 +0300

    page update
    page update

--- internship-project-tracker/src/app/page.js
+++ internship-project-tracker/src/app/page.js
@@ -1,9 +1,9 @@
-import DashboardManager from "@/components/DashboardManager";
+import DashboardManager from '@/components/DashboardManager';
 
 export default function Home() {
   return (
-    <main className="p-4 md:p-8">
-      <div className="max-w-6xl mx-auto">
+    <main className="flex-1 w-full max-w-full m-0 p-4 md:p-8 flex flex-col justify-start items-center overflow-x-hidden">
+      <div className="w-full max-w-6xl mx-auto flex-1 flex flex-col">
         <DashboardManager />
       </div>
     </main>
